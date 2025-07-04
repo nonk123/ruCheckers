a9877dfd87e52c436a51f407f90f3b6bd94d9d10
nonk123 <me@nonk.dev>
2025-07-04 19:00:08 +0300

    Try and get some lobbies

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#include "__gen.h"
 #include "caulk.h"
 #include "raylib.h"
 
 static bool death = false;
 static const char* panic = NULL;
 
+static bool steamInited = false;
+static size_t lobbyCount = 0;
+
 enum {
-	ST_INIT,
 	ST_LOBBIES,
 };
 
@@ -21,24 +25,30 @@ static void exitOnEsc() {
 		death = true;
 }
 
-static size_t curState = ST_INIT;
+static void fillLobbyList(void* data) {
+	lobbyCount = ((LobbyMatchList_t*)data)->m_nLobbiesMatching;
+	printf("\n\nbish %lu\n\n", lobbyCount);
+}
+
+static void requestLobbies() {
+	ISteamMatchmaking* mm = caulk_SteamMatchmaking();
+	caulk_ISteamMatchmaking_AddRequestLobbyListStringFilter(mm, "ruCheckers", "fimoz", k_ELobbyComparisonEqual);
+	SteamAPICall_t cb = caulk_ISteamMatchmaking_RequestLobbyList(mm);
+	caulk_Gucci(cb, fillLobbyList);
+	lobbyCount = 0;
+}
+
+static bool getLobbyId(size_t idx) {
+	if (idx >= lobbyCount)
+		return false;
+	ISteamMatchmaking* mm = caulk_SteamMatchmaking();
+	CSteamID id = caulk_ISteamMatchmaking_GetLobbyByIndex(mm, idx);
+	return true;
+}
+
+static size_t curState = ST_LOBBIES;
 static void setState(int state) {
 	curState = state;
-}
-
-static bool steamInited = false;
-static void initUpdate() {
-	exitOnEsc();
-
-	if (caulk_Init()) {
-		steamInited = true;
-		setState(ST_LOBBIES);
-	} else
-		panic = "Failed to connect to Steam!";
-}
-
-static void initDraw() {
-	ClearBackground(GREEN);
 }
 
 static void lobbiesUpdate() {
@@ -46,11 +56,15 @@ static void lobbiesUpdate() {
 }
 
 static void lobbiesDraw() {
+	const int fs = 40, pad = 5;
+
 	ClearBackground(RAYWHITE);
+	if (!lobbyCount)
+		DrawText("Loading lobbies...", pad, pad, fs, BLACK);
+	DrawText("C to create a lobby", pad, pad + fs, fs, RED);
 }
 
 static const struct State states[] = {
-    [ST_INIT] = {initUpdate, initDraw},
     [ST_LOBBIES] = {lobbiesUpdate, lobbiesDraw},
 };
 
@@ -73,7 +87,16 @@ int main(int argc, char* argv[]) {
 	SetExitKey(KEY_NULL);
 	SetTargetFPS(60);
 
+	if (caulk_Init()) {
+		steamInited = true;
+		requestLobbies();
+	} else
+		panic = "Failed to connect to Steam!";
+
 	while (!death && !WindowShouldClose()) {
+		if (steamInited)
+			caulk_Dispatch();
+
 		const struct State* state = &states[curState];
 		if (panic)
 			panicUpdate();
